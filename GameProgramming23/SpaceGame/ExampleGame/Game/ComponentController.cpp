5e728154badaf7e34daa19b3abd7d552f4b7d1e6
Jacob Rohde <jas.rohde@gmail.com>
2023-09-29 11:23:39 +0200

    cosmetics

--- GameProgramming23/SpaceGame/ExampleGame/Game/ComponentController.cpp
+++ GameProgramming23/SpaceGame/ExampleGame/Game/ComponentController.cpp
@@ -5,8 +5,9 @@
 namespace ExampleGame {
 	void ComponentController::Init() {
 		MyEngine::Engine* engine = MyEngine::Engine::GetInstance();
-		basePos = engine->GetScreenSize() / 2.f;
 
+		basePos = engine->GetScreenSize() / 2.f;
+		
 		MyEngine::GameObject* parent = GetGameObject();
 		parent->position = basePos;
 	}
@@ -16,17 +17,6 @@ namespace ExampleGame {
 		MyEngine::GameObject* parent = GetGameObject();
 
 		parent->rotation += rotSpeed * deltaTime;
-
-
-		/*
-		// rotation is in degrees, need to make it into a glm vector and set movDirection to it 
-		// Convert rotation to radians
-		float rotationRadians = glm::radians(parent->rotation);
-
-		// Convert rotation into a glm vector (unit vector)
-		movDirection.x = glm::cos(rotationRadians);
-		movDirection.y = glm::sin(rotationRadians);
-		*/
 
 		parent->position = parent->position + movDirection * movAmount;
 		//parent->position = basePos + movDirection * movAmount * glm::sin(movSpeed * engine->GetTime());
