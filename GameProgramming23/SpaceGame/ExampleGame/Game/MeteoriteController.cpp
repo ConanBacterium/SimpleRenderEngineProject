831bc252baf453dfc496e545233fd266aca5e583
Roman <58208649+Romes8@users.noreply.github.com>
2023-10-02 15:45:29 +0200

    Meteorite movement improved

--- GameProgramming23/SpaceGame/ExampleGame/Game/MeteoriteController.cpp
+++ GameProgramming23/SpaceGame/ExampleGame/Game/MeteoriteController.cpp
@@ -14,23 +14,41 @@ namespace ExampleGame {
     }
 
     void MeteoriteController::InitMeteor() {
+
         // NEED CONSTRAINT ONLY GO DOWN 
         MyEngine::Engine* engine = MyEngine::Engine::GetInstance();
         MyEngine::GameObject* parent = GetGameObject();
+
+        const float HEIGHT = engine->GetScreenSize().y;
+        const float WIDTH = engine->GetScreenSize().x;
+
 
         ComponentController::rotSpeed = static_cast<float>(rand()) / RAND_MAX * 360.0f;
         ComponentController::movSpeed = 1.0f + static_cast<float>(rand()) / RAND_MAX * 9.0f;
         ComponentController::movAmount = 1.0f + static_cast<float>(rand()) / RAND_MAX * 99.0f;
 
-        float dirX = 2.0f * (static_cast<float>(rand()) / RAND_MAX) - 1.0f;
-        float dirY = -static_cast<float>(rand()) / RAND_MAX;
-        ComponentController::movDirection = glm::normalize(glm::vec2(dirX, dirY));
+        //get me highest y point and generate random x number within the screen range
+        float randomX_start = static_cast<float>(rand()) / RAND_MAX * WIDTH;
+        glm::vec2 startPos = glm::vec2(randomX_start, HEIGHT);
 
-        float posX = static_cast<float>(rand()) / RAND_MAX * 1000.0f;
-        float posY = static_cast<float>(rand()) / RAND_MAX * 1000.0f;
-        ComponentController::basePos = glm::vec2(posX,  engine->GetScreenSize().y);
+        printf("Start pos: x: %f,y: %f \n", randomX_start, HEIGHT);
+
+        //get me the lowest point and get me a random x number within the screen range
+        float randomX_endPos = static_cast<float>(rand()) / RAND_MAX * WIDTH;
+        glm::vec2  endPos = glm::vec2(randomX_endPos, 0.0f);
+        //connect these 2 generated points for a movind direction of an object
+
+        printf("End pos: x: %f,y: %f ", randomX_endPos, 0.0);
+
+
+        ComponentController::movDirection = glm::normalize(endPos - startPos);
+
+
+        ComponentController::basePos = startPos;
 
         parent->position = basePos;
+
+
     }
 
 
